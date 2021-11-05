f841e61b10695d8ed42797475332c170cb437139
张卫华 <21212010055@m.fudan.edu.cn>
2021-11-05 18:24:53 +0800

    2021-11-05

---
+++ Project/eureka-python/BackTest/BackTest.py
@@ -0,0 +1,295 @@
+import json
+import itertools
+import time
+import requests
+import collections
+from datetime import datetime
+from Strategy import BaseStrategy
+from DataSrc import HisQuotes
+
+OPEN = 'OPEN'
+CLOSE = "CLOSE"
+LONG = 'LONG'
+SHORT = 'SHORT'
+
+
+def iterize(iterable):
+    niterable = list()
+    for elem in iterable:
+        if isinstance(elem, str):
+            elem = (elem,)
+        elif not isinstance(elem, collections.Iterable):
+            elem = (elem,)
+
+        niterable.append(elem)
+
+    return niterable
+
+
+class Order(object):
+    def __init__(self, price, volume, direction, operation):
+        super(Order, self).__init__()
+        self.price = price
+        self.volume = volume
+        self.direction = direction
+        self.operation = operation
+
+
+"""
+这个类的目的是：模拟交易所，推送行情给用户（策略），并实现撮合成交
+"""
+
+
+class BackTester(object):
+    def __init__(self):
+        super(BackTester, self).__init__()
+        # 回测策略类
+        self.strategy_class = None
+        # 期货合约
+        self.ts_code = None
+        # 起始日期
+        self.start_date = None
+        # 结束日期
+        self.end_date = None
+        # 初始本金.
+        self.cash = 1_000_000
+        # 策略实例
+        self.strategy_instance = None
+        # 手续费
+        self.commission = 2 / 1000
+        # 杠杆比例，默认使用杠杆
+        self.leverage = 1.0
+        # 滑点率，设置为万5
+        self.slipper_rate = 5 / 10000
+        # 购买的资产的估值，作为计算爆仓的时候使用.
+        self.asset_value = 0
+        # 最低保证金比例, 使用bitfinex 的为例子.
+        self.min_margin_rate = 0.15
+        # 成交单
+        self.trades = []
+        # 报单列表  数据格式是dataframe格式
+        self.active_orders = []
+        # 持多仓列表  数据格式是dataframe格式
+        self.pos_long = []
+        # 持空仓列表  数据格式是dataframe格式
+        self.pos_short = []
+        # 回测的数据 dataframe数据
+        self.backtest_data = None
+        # 是否是运行策略优化的方法。
+        self.is_optimizing_strategy = False
+
+    # 开始回测
+    def start(self):
+        # 先加载数据
+        self.init_data()
+        # 回放数据
+        self.handle_data()
+        # 启动策略
+        self.run()
+        # 回测完成
+        self.finish()
+
+    def init_data(self):
+        """
+        从数据库初始化数据
+        :return:
+        """
+        self.sendInfo("开始加载历史数据")
+        time.sleep(2)
+        with HisQuotes() as hq:
+            self.backtest_data = hq.pullData(ts_code=self.ts_code, start_date=self.start_date, end_date=self.end_date)
+        self.sendInfo("历史数据加载完成")
+
+    # 历史数据回放
+    def handle_data(self):
+        self.sendInfo("开始回放历史数据")
+        time.sleep(5)
+        self.sendInfo("历史数据回放结束")
+
+    def finish(self):
+        """
+        回测结束调用，发送回测指标【资金收益率、最大回撤】等
+        :return:
+        """
+        time.sleep(2)
+        self.sendInfo("回测完成")
+
+    def set_strategy(self, strategy_class: BaseStrategy):
+        """
+        设置要跑的策略类.
+        :param strategy_class:
+        :return:
+        """
+        self.strategy_class = strategy_class
+
+    def set_tsCode(self, ts_code):
+        """
+        设置期货合约
+        :param ts_code: 期货合约
+        :return:
+        """
+        self.ts_code = ts_code
+
+    def set_data(self, start_date, end_date):
+        """
+        设置合约起始结束日期
+        :param start_date: 开始日期
+        :param end_date: 结束日期
+        :return:
+        """
+        self.start_date = start_date
+        self.end_date = end_date
+
+    def set_cash(self, cash):
+        """
+        设置本金
+        :param cash: 本金
+        :return:
+        """
+        self.cash = cash
+
+    def set_leverage(self, leverage: float):
+        """
+        设置杠杆率.
+        :param leverage:
+        :return:
+        """
+        self.leverage = leverage
+
+    def set_commission(self, commission: float):
+        """
+        设置手续费.
+        :param commission:
+        :return:
+        """
+        self.commission = commission
+
+    def buy(self, price, volume):
+        print(f"开多仓下单: {volume}@{price}")
+        order = Order(price, volume, OPEN, LONG)
+        self.active_orders.append(order)
+
+    def sell(self, price, volume):
+        print(f"平多仓下单: {volume}@{price}")  #
+        order = Order(price, volume, CLOSE, LONG)
+        self.active_orders.append(order)
+
+    def short(self, price, volume):
+        print(f"开空仓下单: {volume}@{price}")
+        order = Order(price, volume, OPEN, SHORT)
+        self.active_orders.append(order)
+
+    def cover(self, price, volume):
+        print(f"平空仓下单: {volume}@{price}")
+        order = Order(price, volume, CLOSE, SHORT)
+        self.active_orders.append(order)
+
+    def select_posList(self):
+        return self.pos_long, self.pos_short
+
+    def set_strategy_instance(self, strategy_instance):
+        """
+        设置策略实例
+        :return:
+        """
+        self.strategy_instance = strategy_instance
+
+    def run(self):
+        # self.strategy_instance = self.strategy_class(self.backtest_data)
+        self.strategy_instance.broker = self
+        self.strategy_instance.on_start()
+
+        for index, bar in self.backtest_data.iterrows():
+            # bar = BarData(candle['trade_date'], candle['open'], candle['close'],
+            #               candle['high'], candle['low'], candle['vol'])
+            self.check_order(bar)                   # 检查该行情bar是否满足成交条件
+            self.strategy_instance.on_bar(bar)      # 给到策略（用户）
+
+        self.strategy_instance.on_stop()
+        # 统计成交的信息..,可以先在暂时不考虑
+        # self.calculate()
+
+    def check_order(self, bar):
+        """
+        模拟交易所的撮合成交（根据当前这笔行情bar，判断用户已报的单子是否满足成交条件。如果满足，则成交；不满足，跳出）
+        :param bar:
+        :return:
+        """
+        # 当前这比行情的价格
+        for order in self.active_orders:
+            price = bar.close_price
+            if order.operation == OPEN:
+                if order.direction == LONG and price <= order.price:   # 开多仓
+                    # （1）报单记录去掉该单子（2）持仓记录添加该单子 （3）trades成交单+1（4）处理cash，cash-=成交价格*成交量
+                    self.cash -= order.price * order.volume
+                    self.pos_long.push(order)
+                if order.direction == SHORT and price >= order.price:   # 开空仓单子
+                    self.cash += order.price * order.volume
+                    self.pos_short.push(order)
+            else:
+                if order.direction == LONG and price >= order.prcie:   # 平多仓
+                    self.cash += order.price * order.volume
+                    self.pos_long.pop(order)
+                if order.direction == SHORT and price <= order.price:
+                    self.cash -= order.price * order.volume
+                    self.pos_short.pop(order)
+            # 报单记录都是需要pop掉
+            self.active_orders.pop(order)
+            # 成交单都是要push进来
+            self.trades.push(order)
+
+    def calculate(self):
+        """
+        # 拿到成交的信息，把成交的记录统计出来.夏普率、 盈亏比、胜率、 最大回撤 年化率/最大回撤
+        :return:
+        """
+        for trade in self.trades:
+            """
+             order_id 
+             trade_id
+             price,
+             volume 
+             
+             10000 --> 1BTC
+             12000 --> 1BTC  -- >  2000
+            """
+            pass
+
+    def optimize_strategy(self, **kwargs):
+        """
+        优化策略， 参数遍历进行..，如双均线策略，遍历长短周期值
+        :param kwargs:
+        :return:
+        """
+        self.is_optimizing_strategy = True
+
+        optkeys = list(kwargs)
+        vals = iterize(kwargs.values())
+        optvals = itertools.product(*vals)
+        optkwargs = map(zip, itertools.repeat(optkeys), optvals)
+        optkwargs = map(dict, optkwargs)  # dict value...
+
+        for params in optkwargs:
+            print(params)
+
+        # 参数列表, 要优化的参数, 放在这里.
+        cash = self.cash
+        leverage = self.leverage
+        commission = self.commission
+        for params in optkwargs:
+            self.strategy_class.params = params
+            self.set_cash(cash)
+            self.set_leverage(leverage)
+            self.set_commission(commission)
+            self.run()
+
+    # 获取当前系统时间
+    def getNowTime(self):
+        return datetime.strftime(datetime.now(), "%H:%M:%S")
+
+    def sendInfo(self, info):
+        re = {'info': '[' + self.getNowTime() + ']' + info}
+        url = "http://localhost:5678/sendDoubleMABackTestInfo"
+        headers = {'Content-type': 'application/json'}
+        requests.post(url, data=json.dumps(re), headers=headers)
+
---
+++ Project/eureka-python/BackTest/Strategy.py
@@ -0,0 +1,148 @@
+import pandas as pd
+# 定义几个常量
+SELL = 's'
+BUY = 'b'
+OPEN = 'o'
+CLOSE = 'c'
+
+
+"""
+策略基类：策略类是站在用户的角度，收到行情后什么时候去下单
+"""
+
+
+class BaseStrategy(object):
+    broker = None
+
+    def __init__(self):
+        super(BaseStrategy, self).__init__()
+
+    def on_start(self):
+        """
+        策略开始运行
+        :return:
+        """
+        self.broker.sendInfo("策略开始运行")
+
+    def on_stop(self):
+        """
+        策略运行结束
+        :return:
+        """
+        self.broker.sendInfo("策略运行结束")
+
+    def on_bar(self, bar):
+        raise NotImplementedError("请在子类中实现该方法")
+
+    def buy(self, price, volume):
+        """
+        做多
+        :param price: 价格
+        :param volume: 数量
+        :return:
+        """
+        self.broker.buy(price, volume)
+
+    def sell(self, price, volume):
+        """
+        平多
+        :param price: 价格
+        :param volume: 数量
+        :return:
+        """
+        self.broker.sell(price, volume)
+
+    def short(self, price, volume):
+        """
+        做空
+        :param price: 价格
+        :param volume: 数量
+        :return:
+        """
+        self.broker.short(price, volume)
+
+    def cover(self, price, volume):
+        """
+        平空
+        :param price: 价格
+        :param volume: 数量
+        :return:
+        """
+        self.broker.cover(price, volume)
+
+
+"""
+【双均线策略】 created by ZWH 2021-11-04
+"""
+
+
+class DoubleMovingAverage(BaseStrategy):
+    def __init__(self):
+        super(DoubleMovingAverage, self).__init__()
+        # 长线周期默认10天
+        self.long_term = 10
+        # 短线周期默认5天
+        self.short_term = 5
+        # 缓存的行情数据，根据long_term缓存条数
+        self.bar_list = []
+
+    def set_long(self, long):
+        """
+        设置长线周期
+        :param long:
+        :return:
+        """
+        self.long_term = long
+
+    def set_short(self, short):
+        """
+        设置短线周期
+        :param short:
+        :return:
+        """
+        self.short_term = short
+
+    # 实现父类方法，这里是核心，整个策略都在这里。相当于用户报单的逻辑，就是实现什么时候报单、报哪种单(开、平)
+    def on_bar(self, bar):
+        """
+        【策略逻辑】：(1) 当短期均线由下向上穿越长期均线时做多；(2) 当短期均线由上向下穿越长期均线时做空。
+        【解释】：（1）短期均线上穿长期均线时：如果没有持空仓，则报（开多仓）单；如果有持空仓，则平空仓。
+                （2）短期均线下穿长期均线时：如果没有持多仓，则报（开空仓）单；如果有持多仓，则平多仓。
+        【双均线，当缓存到long_term的行情数据时，计算5日均线和10日均线，再往后收到行情判断策略逻辑即可】
+        :param bar:
+        :return:
+        """
+        # （1）获取bar
+        print("双均线收到行情：{}".format(bar))
+        # （2）bar推送到缓存bar_list
+        self.bar_list.push(bar)
+        # （3）判断bar_list的数据是否足够10条，如果不足够，则不做处理；如果足够，进入（4）
+        if len(self.bar_list < 10):
+            return
+        # （4）取bar_list的最后long_term个元素：bar_list = bat_list[-self.long_term:]
+        self.bar_list = self.bar_list[-self.long_term:]
+        # （4）计算barlist的5日均线和10日均线
+        short_avg = round(self.bar_list.close.rolling(self.short_term, min_periods=1).mean(), 2)
+        long_avg = round(self.bar_list.close.rolling(self.short_term, min_periods=1).mean(), 2)
+        # （5）查询持仓
+        pos_long, pos_short = self.broker.select_posList()
+        # （6）双均线逻辑
+        order_price = self.bar_list[-2].close
+        # 短均线下穿长均线，做空(即当前时间点短均线处于长均线下方，前一时间点短均线处于长均线上方)
+        if long_avg[-2] < short_avg[-2] and long_avg[-1] >= short_avg[-1]:
+            # 无多仓持仓情况下，直接开空
+            if not pos_long:
+                self.short(price=order_price, volume=1)
+            # 有多仓情况下，先平多，再开空(开空命令放在on_order_status里面)
+            else:
+                # 以市价平多仓
+                self.sell(price=order_price, volume=1)
+        # 短均线上穿长均线，做多（即当前时间点短均线处于长均线上方，前一时间点短均线处于长均线下方）
+        if short_avg[-2] < long_avg[-2] and short_avg[-1] >= long_avg[-1]:
+            # 无空仓情况下，直接开多
+            if not pos_short:
+                self.buy(price=order_price, volume=1)
+            # 有空仓的情况下，先平空，再开多(开多命令放在on_order_status里面)
+            else:
+                # 以市价平空仓
+                self.cover(price=order_price, volume=1)
