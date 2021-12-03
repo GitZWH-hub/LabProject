e3ded82ba6c41937151fa1f3b30f3d9b8f1898cf
张卫华 <21212010055@m.fudan.edu.cn>
2021-12-03 18:04:08 +0800

    2021-12-03

--- Project/gateway/src/main/java/com/naah/gateway/filter/AuthGlobalFilter.java
+++ Project/gateway/src/main/java/com/naah/gateway/filter/AuthGlobalFilter.java
@@ -2,6 +2,7 @@ package com.naah.gateway.filter;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
@@ -54,24 +55,39 @@ import java.io.IOException;
 ////        };
 ////    }
 //}
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+/**
+ * 自定义全局过滤器，需要实现GlobalFilter和Ordered接口
+ */
 @Component
-public class CorsFilter implements Filter {
+public class AuthGlobalFilter implements GlobalFilter, Ordered {
+    private static final String OPTIONS = "OPTIONS";
 
     @Override
-    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
-        HttpServletResponse response = (HttpServletResponse) res;
-        response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE, PATCH");
-        response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-        response.setHeader("Access-Control-Expose-Headers", "Location");
-        chain.doFilter(req, res);
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        if (OPTIONS.equalsIgnoreCase(String.valueOf(exchange.getRequest().getMethod()))) {
+            System.out.println("OPTIONS鉴定...");
+            exchange.getResponse().setStatusCode(HttpStatus.OK);
+            return exchange.getResponse().setComplete();
+        }
+        // 继续执行filter链
+        return chain.filter(exchange);
     }
 
+    // 顺序，数值越小，优先级越高
     @Override
-    public void init(FilterConfig filterConfig) {}
-
-    @Override
-    public void destroy() {}
-
+    public int getOrder() {
+        return 0;
+    }
 }
+
+
