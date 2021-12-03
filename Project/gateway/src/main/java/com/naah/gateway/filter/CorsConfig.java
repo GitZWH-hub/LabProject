0076bec0b071da12f7f6d5696cc82b143a1bd95b
张卫华 <21212010055@m.fudan.edu.cn>
2021-12-03 23:10:54 +0800

    2021-12-03 add CorsConfig

---
+++ Project/gateway/src/main/java/com/naah/gateway/filter/CorsConfig.java
@@ -0,0 +1,31 @@
+package com.naah.gateway.filter;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.util.pattern.PathPatternParser;
+
+/**
+ * @ClassName CorsConfig
+ * @Description gateway跨域配置
+ * @Date 2019/9/5 14:00
+ **/
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsWebFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedMethod("*");
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsWebFilter(source);
+    }
+}
