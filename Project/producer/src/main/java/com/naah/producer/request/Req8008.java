4c5bcf9990559788d8c9743431a3a32e41be9c8d
张卫华 <21212010055@m.fudan.edu.cn>
2021-12-03 15:02:19 +0800

    2021-12-03

---
+++ Project/producer/src/main/java/com/naah/producer/request/Req8008.java
@@ -0,0 +1,39 @@
+package com.naah.producer.request;
+
+
+/*
+    Req8001: Req pull data from TuShare
+ */
+
+public class Req8008 {
+
+    private String fut;
+
+    private String start;
+
+    private String end;
+
+    public String getFut() {
+        return fut;
+    }
+
+    public String getStart() {
+        return start;
+    }
+
+    public String getEnd() {
+        return end;
+    }
+
+    public void setFut(String fut) { this.fut = fut; }
+
+    public void setStart(String start) {
+        this.start = start;
+    }
+
+    public void setEnd(String end) {
+        this.end = end;
+    }
+
+}
+
