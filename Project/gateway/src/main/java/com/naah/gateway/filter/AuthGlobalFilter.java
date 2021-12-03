8cddc40e3c7c715585d67a10b549f7c929a58c0a
张卫华 <21212010055@m.fudan.edu.cn>
2021-12-03 19:30:14 +0800

    2021-12-03

--- Project/gateway/src/main/java/com/naah/gateway/filter/AuthGlobalFilter.java
+++ Project/gateway/src/main/java/com/naah/gateway/filter/AuthGlobalFilter.java
@@ -1,5 +1,6 @@
 package com.naah.gateway.filter;
 
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
@@ -7,6 +8,9 @@ import org.springframework.core.Ordered;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.core.io.buffer.DataBuffer;
+import java.nio.charset.StandardCharsets;
 
 /**
  * 自定义全局过滤器，需要实现GlobalFilter和Ordered接口
@@ -17,10 +21,16 @@ public class AuthGlobalFilter implements GlobalFilter, Ordered {
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        ServerHttpResponse response = exchange.getResponse();
         if (OPTIONS.equalsIgnoreCase(String.valueOf(exchange.getRequest().getMethod()))) {
+            JSONObject message = new JSONObject();
+            message.put("status", 200);
+            message.put("data", "YES");
             System.out.println("OPTIONS鉴定...");
-            exchange.getResponse().setStatusCode(HttpStatus.OK);
-            return exchange.getResponse().setComplete();
+            byte[] bits = message.toJSONString().getBytes(StandardCharsets.UTF_8);
+            DataBuffer buffer = response.bufferFactory().wrap(bits);
+            response.setStatusCode(HttpStatus.OK);
+            return response.writeWith(Mono.just(buffer));
         }
         System.out.println(String.valueOf(exchange.getRequest().getMethod()));
         // 继续执行filter链
