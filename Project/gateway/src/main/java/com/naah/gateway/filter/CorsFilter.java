3e28901d917a83870e89fc0f034743d4116d4899
张卫华 <21212010055@m.fudan.edu.cn>
2021-12-03 15:28:35 +0800

    2021-12-03

---
+++ Project/gateway/src/main/java/com/naah/gateway/filter/CorsFilter.java
@@ -0,0 +1,36 @@
+package com.naah.gateway.filter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebFilter(filterName = "CorsFilter", urlPatterns = "/*")
+public class CorsFilter implements Filter {
+    private static final String OPTIONS = "OPTIONS";
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        response.addHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "POST,GET,PUT,DELETE,OPTIONS");
+        response.addHeader("Access-Control-Allow-Headers", "*");
+        //response.addHeader("Access-Control-Max-Age", "3628800"); //可选
+        if(OPTIONS.equalsIgnoreCase(request.getMethod()))
+            return;  // 或者直接输入204、HttpStatus.SC_OK、200，等这些都可以   import org.apache.http.HttpStatus;
+        filterChain.doFilter(servletRequest, response);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
+
