c8d55577ee89e20dc6082bb2bbb86252f166442f
张卫华 <21212010055@m.fudan.edu.cn>
2021-12-03 16:37:19 +0800

    2021-12-03

--- Project/gateway/src/main/java/com/naah/gateway/filter/CorsFilter.java
+++ Project/gateway/src/main/java/com/naah/gateway/filter/CorsFilter.java
@@ -1,11 +1,14 @@
 package com.naah.gateway.filter;
 
+import org.springframework.stereotype.Component;
+
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@Component
 @WebFilter(filterName = "CorsFilter", urlPatterns = "/*")
 public class CorsFilter implements Filter {
     private static final String OPTIONS = "OPTIONS";
