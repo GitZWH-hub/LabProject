b9286582e54c9ebed0985c2c77be5f319cac9318
张卫华 <21212010055@m.fudan.edu.cn>
2021-12-17 17:37:09 +0800

    数据挖掘课程

--- Project/agent-bullet/src/main/java/com/naah/bullet/request/req9999.java
+++ Project/agent-bullet/src/main/java/com/naah/bullet/request/req9999.java
@@ -2,12 +2,20 @@ package com.naah.bullet.request;
 
 public class req9999 {
     String info;
-
+    float mse;
     public String getInfo() {
         return info;
     }
 
     public void setInfo(String info) {
         this.info = info;
+    }
+
+    public float getMse() {
+        return mse;
+    }
+
+    public void setMse(float mse) {
+        this.mse = mse;
     }
 }
