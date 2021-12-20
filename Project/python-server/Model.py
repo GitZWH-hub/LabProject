7c8fd7f03535c25fe8188497db4c9c6d6334aa09
张卫华 <21212010055@m.fudan.edu.cn>
2021-12-20 15:43:45 +0800

    数据挖掘课程

--- Project/python-server/Model.py
+++ Project/python-server/Model.py
@@ -144,10 +144,13 @@ class Model(object):
         self.mse = round(metrics.mean_squared_error(self.y_test, pred_y),2)
 
         y_test = self.y_test.reset_index(drop=True)
+        plt.figure(figsize=(8, 4), frameon=False)
         plt.plot(pred_y[:99], label=u'predict')
         plt.plot(y_test[:99], label=u'true')
         plt.ylabel(u"Credit score")
         plt.title(u"LGBM Model (top 100) " + get_now())
+        # 设置网格线
+        plt.grid(True, ls=':', color='r', alpha=0.5)
         plt.legend()
 
         sio = BytesIO()
@@ -182,7 +185,7 @@ class Model(object):
         plt.plot(val_stack[:99], label=u'predict')
         plt.plot(y_test[:99], label=u'true')
         plt.ylabel(u"Credit score")
-        plt.title(u"xgboost+LGBM Model (top 200) " + get_now())
+        plt.title(u"xgboost+LGBM Model (top 100) " + get_now())
         # 设置网格线
         plt.grid(True, ls=':', color='r', alpha=0.5)
         plt.legend()
