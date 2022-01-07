a8631d1ca7f7faec331a0b2ff4066c571146fdf9
张卫华 <21212010055@m.fudan.edu.cn>
2022-01-07 15:13:50 +0800

    2022-01-07

--- Project/python-server/DataSrc.py
+++ Project/python-server/DataSrc.py
@@ -126,6 +126,7 @@ class HisQuotes(Base):
             # 查询期货合约信息表,查询时间区间内的所有合约代码，全拉下来
             with Futures() as future:
                 ts_codes = future.get_ts_code_by_year(start_date, end_date)
+                print(ts_codes)
                 for code in ts_codes:
                     data = self.pro.fut_daily(ts_code=code)
                     data.to_sql(ts_code[:2].upper(), self.conn, index=True, if_exists='append')
