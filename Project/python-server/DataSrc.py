729e8657725bfaed4bd6eb7dc0fcf99cbad76b1b
张卫华 <21212010055@m.fudan.edu.cn>
2022-01-18 13:27:34 +0800

    2022-01-18

--- Project/python-server/DataSrc.py
+++ Project/python-server/DataSrc.py
@@ -6,7 +6,6 @@ import sqlite3 as sql3
 import logging
 from datetime import datetime
 
-
 '''
 基类
 Created on Sept 27, 2021    @Author: zwh
@@ -21,7 +20,27 @@ class Base(object):
         self.DBNAME = 'DBData'
 
     def __enter__(self):
-        logging.basicConfig(level=logging.NOTSET)  # 设置日志级别
+        logger = logging.getLogger("simple_example")
+        logger.setLevel(logging.DEBUG)
+        # 建立一个filehandler来把日志记录在文件里，级别为debug以上
+        fh = logging.FileHandler("spam.log")
+        fh.setLevel(logging.DEBUG)
+        # 建立一个streamhandler来把日志打在CMD窗口上，级别为error以上
+        ch = logging.StreamHandler()
+        ch.setLevel(logging.ERROR)
+        # 设置日志格式
+        formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+        ch.setFormatter(formatter)
+        fh.setFormatter(formatter)
+        #将相应的handler添加在logger对象中
+        logger.addHandler(ch)
+        logger.addHandler(fh)
+        # 开始打日志
+        logger.debug("debug message")
+        logger.info("info message")
+        logger.warn("warn message")
+        logger.error("error message")
+        logger.critical("critical message")
         self.conn = sql3.connect(self.DBNAME)
 
     def __exit__(self, exc_type, exc_val, exc_tb):
